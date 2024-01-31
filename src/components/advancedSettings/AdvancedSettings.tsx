2ee94cff4114274122ad4d5223bed982639aacf9
Kristen Doucette <cosmicnoir90@gmail.com>
2024-01-30 19:30:56 -0600

    - Add tooltip component + add to advanced settings - Fix Spawn Interval dispaly value to be more in line with slider - Fix some styling + remove some unnecessary rules repeated on letters/words

--- src/components/advancedSettings/AdvancedSettings.tsx
+++ src/components/advancedSettings/AdvancedSettings.tsx
@@ -7,6 +7,7 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Slider from "@mui/material/Slider";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import { TooltipCustom } from "../tooltip";
 
 import "./advancedSettings.sass";
 
@@ -31,57 +32,88 @@ const AdvancedSettings: React.FC<AdvancedSettingsProps> = ({
   const displayScrollSpeed = 11 - scrollSpeed;
 
   // Inverts the range from 900-2000 to 2000-900 for display
-  const spawnIntervalValue = 2900 - spawnInterval;
-  const seconds = spawnIntervalValue / 1000;
-  const displaySpawnInterval = seconds.toFixed(2);
 
+  const maxInterval = 2000;
+  const minInterval = 900;
+  const spawnIntervalSliderValue = maxInterval - (spawnInterval - minInterval);
+  const displaySpawnInterval = (
+    (maxInterval - spawnIntervalSliderValue + minInterval) /
+    1000
+  ).toFixed(2);
   return (
     <Accordion className="advanced-settings">
       <AccordionSummary
         expandIcon={<ExpandMoreIcon style={{ color: "white" }} />}
       >
-        <Typography className="advanced-settilngs_accodion-title">
+        <Typography className="advanced-settilngs_accodion-title space-themed-header-text">
           Advanced Settings
         </Typography>
       </AccordionSummary>
-      <p className="advanced-settings_description">
+      <p className="advanced-settings_description space-themed-text">
         Each difficulty comes with preset values for the below settings. Adjust
         each setting for a more customized adventure.
       </p>
       <AccordionDetails className="advanced-settings_details">
-        <Typography>Scroll Speed - {displayScrollSpeed}</Typography>
-        <Slider
-          className="advanced-settings_slider"
-          value={displayScrollSpeed}
-          onChange={(e, value) =>
-            handleScrollSpeedChange(11 - (value as number))
-          }
-          step={1}
-          marks
-          min={3}
-          max={8}
-        />
-        <Typography>Spawn Interval - {displaySpawnInterval} seconds</Typography>
-        <Slider
-          className="advanced-settings_slider"
-          value={spawnIntervalValue}
-          onChange={(_e, value) =>
-            handleSpawnIntervalChange(2900 - (value as number))
-          }
-          step={100}
-          marks
-          min={900}
-          max={2000}
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={autoSpawnEnabled}
-              onChange={(e) => handleAutoSpawnChange(e.target.checked)}
+        <div className="advanced-settings_section">
+          <Typography className="advanced-settings_label-container">
+            Scroll Speed: {displayScrollSpeed}
+            <TooltipCustom
+              title={`Scroll Speed - determines how quickly words and letters are scrolled across the 
+                    screen towards the player. The higher the speed, the faster a player must type.`}
             />
-          }
-          label="Rapid Spawn"
-        />
+          </Typography>
+          <Slider
+            className="advanced-settings_slider"
+            value={displayScrollSpeed}
+            onChange={(e, value) =>
+              handleScrollSpeedChange(11 - (value as number))
+            }
+            step={1}
+            marks
+            min={3}
+            max={8}
+          />
+        </div>
+        <div className="advanced-settings_section">
+          <Typography className="advanced-settings_label-container">
+            Spawn Interval - {displaySpawnInterval} seconds
+            <TooltipCustom
+              title={`Spawn Interval - determines how often new words and and characters are added to
+                    the game. The smaller the interval, the faster the player must type.`}
+            />
+          </Typography>
+          <Slider
+            className="advanced-settings_slider"
+            value={spawnIntervalSliderValue}
+            onChange={(_e, value) => {
+              const actualValue = maxInterval - (value as number) + minInterval;
+              handleSpawnIntervalChange(actualValue);
+            }}
+            step={100}
+            marks
+            min={900}
+            max={2000}
+          />
+        </div>
+        <div className="advanced-settings_section">
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={autoSpawnEnabled}
+                onChange={(e) => handleAutoSpawnChange(e.target.checked)}
+              />
+            }
+            label={
+              <span>
+                Rapid Spawn
+                <TooltipCustom
+                  title={`Rapid Spawn - determines if a new word or character should be automatically
+                          added when there are no other words or letters. Makes the game more difficult.`}
+                />
+              </span>
+            }
+          />
+        </div>
       </AccordionDetails>
     </Accordion>
   );
